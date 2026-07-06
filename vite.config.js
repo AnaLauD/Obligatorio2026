80f8a5184d4a99f831bc5a0c80e276d0834b11be
AnaLauD <166624269+AnaLauD@users.noreply.github.com>
2026-07-05 21:22:00 -0300

    3

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 export default {
   preview: {
-    allowedHosts: ['https://obligatorio2026-jwsn.onrender.com']
+    allowedHosts: ['obligatorio2026-jwsn.onrender.com']
   },
   server: {
-    allowedHosts: ['https://obligatorio2026-jwsn.onrender.com']
+    allowedHosts: ['obligatorio2026-jwsn.onrender.com']
   }
 }
